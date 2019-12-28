b649d8384e8a91a8df7a652114667a371277940c
scozirge <scozirge2@gmail.com>
2019-12-28 14:22:24 +0800

    test

---
+++ 資料/作品集/作品集壓縮/自製工具/RGZ_改SD檔名工具.bat
@@ -0,0 +1,15 @@
+rename 01.png w-1.png 
+rename 02.png w-2.png 
+rename 03.png w-3.png 
+rename 04.png w-4.png 
+rename 05.png r-1.png 
+rename 06.png r-2.png 
+rename 07.png r-3.png 
+rename 08.png r-4.png 
+rename 09.png a-1.png 
+rename 10.png a-2.png 
+rename 11.png a-3.png 
+rename 12.png a-4.png 
+rename 13.png h-1.png 
+rename 14.png c-1.png 
+
---
+++ 資料/作品集/程式相關作品/自製工具/RGZ_改立繪檔名工具.bat
@@ -0,0 +1,19 @@
+mkdir face
+rename SFace.tga small.tga
+move /-y "small.tga" "face\"
+move /-y "large-1.tga" "face\"
+move /-y "large-2.tga" "face\"
+move /-y "large-3.tga" "face\"
+move /-y "large-4.tga" "face\"
+move /-y "tip.tga" "face\"
+
+
+mkdir photo
+rename 01.tga large-1.tga
+rename 02.tga large-2.tga
+rename 03.tga large-3.tga
+rename 04.tga large-4.tga
+move /-y "large-1.tga" photo\"
+move /-y "large-2.tga" photo\"
+move /-y "large-3.tga" photo\"
+move /-y "large-4.tga" photo\"
